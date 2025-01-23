874e7e274be7839f59c8db8d189b9e14625314e8
totoo7 <stoyanorozov@gmail.com>
2025-01-23 13:31:05 +0200

    Cleanup

--- main.cpp
+++ main.cpp
@@ -1,11 +1,5 @@
 #include "Game.hpp"
-
-const int FPS = 60;
-const int FRAME_DELAY = 1000 / FPS;
-
 int main(int argc, char* argv[]) {
-    uint32_t frame_start;
-    uint32_t frame_time;
     Game game;
     if (!game.init()) {
         SDL_Log("Failed to initialize the game!");
